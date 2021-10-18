cd404e0fdcd727a5d3068a71997bb4fe56d82a8f
damccull <401104+damccull@users.noreply.github.com>
2021-10-17 21:30:13 -0400

    Add application module and main fn to call it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() {
+    // Set up logging using tracing, tracing-subscriber, and tracing-bunyan-formatter
+    let subscriber = get_subscriber("ocieguide".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    // Read the configuration, panicking if it can't be read
+    let configuration = get_configuration().expect("Failed to read the configuration.");
+
+    // Create the application and launch it
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
