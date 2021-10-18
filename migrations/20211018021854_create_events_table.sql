f5ee9e265f25ba69bb33239643a0120e47c84e51
damccull <damccull+git@gmail.com>
2021-10-17 22:34:17 -0400

    Fix a typo in the migration script.

--- migrations/20211018021854_create_events_table.sql
+++ migrations/20211018021854_create_events_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE events(
     id uuid NOT NULL,
     PRIMARY KEY (id),
     stream_id uuid NOT NULL,
-    event_data TEXT NOT NULL
+    event_data TEXT NOT NULL,
     occurred_at timestamptz NOT NULL
 );
