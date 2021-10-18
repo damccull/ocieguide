eeeedfb6c2d30075e384ac936d0bba1b339a3621
damccull <401104+damccull@users.noreply.github.com>
2021-10-17 22:14:13 -0400

    Import rest of needed things and set correct return type.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use ocieguide::{application::Application, configuration::get_configuration};
+use ocieguide::{
+    application::Application,
+    configuration::get_configuration,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[actix_web::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     // Set up logging using tracing, tracing-subscriber, and tracing-bunyan-formatter
     let subscriber = get_subscriber("ocieguide".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
