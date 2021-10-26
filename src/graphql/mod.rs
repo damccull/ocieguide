6fd90b829dc2fd1c440e7706e8c0f7a850fed1b6
damccull <damccull+git@gmail.com>
2021-10-26 09:18:01 -0400

    Disable ocieguide_schema, add ocieguide

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,4 +1,6 @@
-pub mod ocieguide_model;
-pub mod ocieguide_schema;
+// pub mod ocieguide_model;
+// pub mod ocieguide_schema;
 pub mod starwars_model;
 pub mod starwars_schema;
+pub mod ocieguide;
+
