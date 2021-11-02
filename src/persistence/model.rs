e250cf1010c137d57348eb497e203b93ccaf0e9f
damccull <401104+damccull@users.noreply.github.com>
2021-11-02 16:05:57 -0400

    Change error reporting type

--- src/persistence/model.rs
+++ src/persistence/model.rs
@@ -3,6 +3,8 @@ use regex::Regex;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+use crate::error_handling::error_chain_fmt;
+
 pub struct OcieItemEntity {
     id: Uuid,
     pub(crate) nsn: NationalStockNumber,
@@ -13,14 +15,15 @@ pub struct OcieItemEntity {
     price: Option<BigDecimal>,
 }
 
-
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct NationalStockNumber(String);
 impl NationalStockNumber {
-    pub fn parse(value: String) -> Result<Self, String> {
+    pub fn parse(value: String) -> Result<Self, anyhow::Error> {
         let nsn_regex = Regex::new(r#"^\d{4}-\d{2}-\d{3}-\d{4}$"#).unwrap();
         if !nsn_regex.is_match(&value) {
-            return Err("The NSN was not properly formatted.".to_string());
+            return Err(anyhow::anyhow!(
+                "The NSN was not properly formatted.".to_string()
+            ));
         }
         Ok(Self(value))
     }
@@ -34,10 +37,12 @@ impl AsRef<str> for NationalStockNumber {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct LineItemNumber(String);
 impl LineItemNumber {
-    pub fn parse(value: String) -> Result<Self, String> {
+    pub fn parse(value: String) -> Result<Self, anyhow::Error> {
         let lin_regex = Regex::new(r#"^[a-zA-Z0-9]{1}\d{5}$"#).unwrap();
         if !lin_regex.is_match(&value) {
-            return Err("The LIN was not properly formatted.".to_string());
+            return Err(anyhow::anyhow!(
+                "The LIN was not properly formatted.".to_string()
+            ));
         }
         Ok(Self(value))
     }
