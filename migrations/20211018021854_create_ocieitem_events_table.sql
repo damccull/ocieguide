081fc06368376866579229c79db350cc8f92b025
damccull <401104+damccull@users.noreply.github.com>
2021-11-02 09:46:47 -0400

    Make fields non-empty

--- migrations/20211018021854_create_ocieitem_events_table.sql
+++ migrations/20211018021854_create_ocieitem_events_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE ocieitems(
     id uuid NOT NULL,
     PRIMARY KEY (id),
-    nsn TEXT,
-    lin TEXT,
-    nomenclature TEXT,
+    nsn TEXT NOT NULL,
+    lin TEXT NOT NULL,
+    nomenclature TEXT NOT NULL,
     size TEXT,
     unit_of_issue TEXT,
     price DECIMAL
