db4c9f6feb8322c793a00d7be5511bf7ba403f90
damccull <damccull+git@gmail.com>
2022-03-02 15:58:34 -0500

    Get a static health check working.

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,3 +1,17 @@
+use async_graphql::{EmptyMutation, EmptySubscription, MergedObject, SchemaBuilder, Schema};
+
+
 // pub mod ocieguide_model;
 // pub mod ocieguide_schema;
-pub mod ocieguide;
+pub mod health;
+//pub mod ocieguide;
+
+
+pub type OcieGuideSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+
+#[derive(Default, MergedObject)]
+pub struct Query(health::HealthQuery);
+
+pub fn build_schema() -> OcieGuideSchema {
+    Schema::build(Query::default(), EmptyMutation, EmptySubscription).finish()
+}
