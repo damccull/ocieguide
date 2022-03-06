619f8791c3d8f23e3d1e130a96d0383e95d000da
damccull <damccull+git@gmail.com>
2022-03-05 20:19:00 -0500

    Begin writing seed function

---
+++ src/persistence/seed.rs
@@ -0,0 +1,13 @@
+struct OcieJsonItem {
+    lin: String,
+    ets: bool,
+    nsn: String,
+    nomenclature: String,
+    size: String,
+    ui: String,
+    unit_price: f32,
+}
+
+pub fn seed_database() {
+    // Read cif_prices.json and insert data to databasd
+}
