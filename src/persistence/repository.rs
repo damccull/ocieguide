ff8942c9e4056a44b27480fa75332128823b46e9
damccull <damccull+git@gmail.com>
2022-03-14 16:28:49 -0400

    Complete get_alll and get for postgres repository

--- src/persistence/repository.rs
+++ src/persistence/repository.rs
@@ -1,6 +1,7 @@
 use async_trait::async_trait;
-use sqlx::PgPool;
+use bigdecimal::BigDecimal;
 use sqlx::postgres::PgRow;
+use sqlx::PgPool;
 use uuid::Uuid;
 
 use super::model::OcieItem;
@@ -36,31 +37,33 @@ impl OcieItemRepository for PostgresOcieItemRepository {
 
     type RecordIdType = Uuid;
 
-    #[tracing::instrument(name="API V1 - get_all",skip(self,conn))]
+    #[tracing::instrument(name = "API V1 - get_all", skip(self, conn))]
     async fn get_all(&self, conn: &Self::Connection) -> Result<Vec<OcieItem>, Self::Error> {
         let result = sqlx::query!(
             r#"SELECT id, nsn, lin, nomenclature, size, unit_of_issue, price
             FROM ocieitems"#
         )
         .fetch_all(conn)
-        .await?
+        .await
+        .map_err(|e| RepositoryError::DatabaseError(e.into()))
         .into_iter()
+        .flatten()
         .flat_map(|row| {
             let nsn = match NationalStockNumber::parse(row.nsn) {
                 Ok(nsn) => nsn,
                 Err(e) => {
                     tracing::error!("Error parsing NSN: {:?}", e);
-                    return None;
+                    return Err(e);
                 }
             };
             let lin = match LineItemNumber::parse(row.lin) {
                 Ok(lin) => lin,
                 Err(e) => {
                     tracing::error!("Error parsing LIN: {:?}", e);
-                    return None;
+                    return Err(e);
                 }
             };
-            Some(OcieItem {
+            Ok(OcieItem {
                 id: row.id,
                 nsn,
                 lin,
@@ -74,80 +77,72 @@ impl OcieItemRepository for PostgresOcieItemRepository {
         Ok(result)
     }
 
-    #[tracing::instrument(name="API V1 - get",skip(self,conn))]
+    #[tracing::instrument(name = "API V1 - get", skip(self, conn))]
     async fn get(
         &self,
         conn: Self::Connection,
         id: Self::RecordIdType,
     ) -> Result<OcieItem, Self::Error> {
-        let result = sqlx::query!(
+        struct Row {
+            id: Uuid,
+            nsn: String,
+            lin: String,
+            nomenclature: String,
+            size: Option<String>,
+            unit_of_issue: Option<String>,
+            price: Option<BigDecimal>,
+        }
+
+        let result = sqlx::query_as!(
+            Row,
             r#"SELECT id, nsn, lin, nomenclature, size, unit_of_issue, price
             FROM ocieitems
             WHERE id = $1"#,
             id
-        ).map(|row| -> Result<_,anyhow::Error>{
-                let nsn = match NationalStockNumber::parse(row.nsn) {
-                    Ok(nsn) => nsn,
-                    Err(e) => {
-                        tracing::error!("Error parsing NSN: {:?}", e);
-                        return Err(e);
-                    }
-                };
-                let lin = match LineItemNumber::parse(row.lin) {
-                    Ok(lin) => lin,
-                    Err(e) => {
-                        tracing::error!("Error parsing LIN: {:?}", e);
-                        return Err(e);
-                    }
-                };
-                Ok(OcieItem {
-                    id: row.id,
-                    nsn,
-                    lin,
-                    nomenclature: row.nomenclature,
-                    size: row.size,
-                    unit_of_issue: row.unit_of_issue,
-                    price: row.price,
-                })
+        )
+        .map(|row| {
+            let nsn = match NationalStockNumber::parse(row.nsn) {
+                Ok(nsn) => nsn,
+                Err(e) => {
+                    tracing::error!("Error parsing NSN: {:?}", e);
+                    return Err(e);
+                }
+            };
+            let lin = match LineItemNumber::parse(row.lin) {
+                Ok(lin) => lin,
+                Err(e) => {
+                    tracing::error!("Error parsing LIN: {:?}", e);
+                    return Err(e);
+                }
+            };
+            Ok(OcieItem {
+                id: row.id,
+                nsn,
+                lin,
+                nomenclature: row.nomenclature,
+                size: row.size,
+                unit_of_issue: row.unit_of_issue,
+                price: row.price,
             })
-        .fetch_one(&conn).await?;
-        //.await?;
-        // .into_iter()
-        // .flat_map(|row| {
-        //     let nsn = match NationalStockNumber::parse(row.nsn) {
-        //         Ok(nsn) => nsn,
-        //         Err(e) => {
-        //             tracing::error!("Error parsing NSN: {:?}", e);
-        //             return None;
-        //         }
-        //     };
-        //     let lin = match LineItemNumber::parse(row.lin) {
-        //         Ok(lin) => lin,
-        //         Err(e) => {
-        //             tracing::error!("Error parsing LIN: {:?}", e);
-        //             return None;
-        //         }
-        //     };
-        //     Some(OcieItem {
-        //         id: row.id,
-        //         nsn,
-        //         lin,
-        //         nomenclature: row.nomenclature,
-        //         size: row.size,
-        //         unit_of_issue: row.unit_of_issue,
-        //         price: row.price,
-        //     })
-        // })
-        // .collect::<Vec<OcieItem>>();
-        result
+        })
+        .fetch_one(&conn)
+        .await;
+
+        match result {
+            Ok(r) => match r {
+                Ok(r) => Ok(r),
+                Err(e) => return Err(RepositoryError::UnexpectedError(e)),
+            },
+            Err(e) => Err(RepositoryError::DatabaseError(e.into())),
+        }
     }
 
-    #[tracing::instrument(name="API V1 - add",skip(self,conn))]
+    #[tracing::instrument(name = "API V1 - add", skip(self, conn))]
     async fn add(&self, conn: Self::Connection, item: OcieItem) -> Result<OcieItem, Self::Error> {
         todo!()
     }
 
-    #[tracing::instrument(name="API V1 - update",skip(self,conn))]
+    #[tracing::instrument(name = "API V1 - update", skip(self, conn))]
     async fn update(
         &self,
         conn: Self::Connection,
