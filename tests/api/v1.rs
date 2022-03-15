7a6b4608a71b0d318a72ccc173724b7c20cfac8b
damccull <401104+damccull@users.noreply.github.com>
2022-03-15 18:09:19 -0400

    Update repo

--- tests/api/v1.rs
+++ tests/api/v1.rs
@@ -1,4 +1,4 @@
-use ocieguide::persistence::model::{OcieItem, NationalStockNumber};
+use ocieguide::persistence::model::{LineItemNumber, NationalStockNumber, OcieItem};
 use uuid::Uuid;
 
 use crate::helpers::TestApp;
@@ -6,8 +6,16 @@ use crate::helpers::TestApp;
 #[actix_rt::test]
 async fn add_persists_item_into_respository() {
     // Arrange
-    let app = spawn_app().await;
-    let item = OcieItem { id: Uuid::new_v4(), nsn: NationalStockNumber("010-0000-00000-0000"), lin: LineItemNumber("N12345"), nomenclature: todo!(), size: todo!(), unit_of_issue: todo!(), price: todo!() }
+    let app = TestApp::spawn().await;
+    let item = OcieItem {
+        id: Uuid::new_v4(),
+        nsn: NationalStockNumber::parse("010-0000-00000-0000".into()).unwrap(),
+        lin: LineItemNumber::parse("N12345".into()).unwrap(),
+        nomenclature: todo!(),
+        size: todo!(),
+        unit_of_issue: todo!(),
+        price: todo!(),
+    };
 
     // Act
 
@@ -30,4 +38,3 @@ async fn get_all_returns_all_records() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-
