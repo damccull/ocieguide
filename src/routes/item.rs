3ea3f955b1c740929591036caf435181c63ae283
damccull <401104+damccull@users.noreply.github.com>
2024-01-14 12:07:24 -1000

    Simulate returning json result from database

--- src/routes/item.rs
+++ src/routes/item.rs
@@ -3,6 +3,18 @@ use actix_web::{get, web::Path, HttpResponse, Responder};
 #[get("/item/{id}")]
 #[tracing::instrument]
 pub async fn item(id: Path<i32>) -> impl Responder {
-    let message = format!("Item list for {}", id.as_ref());
-    HttpResponse::Ok().body(message)
+    let item = serde_json::json!(
+          {
+            "id": id.as_ref(),
+            "lin": "04071N",
+            "ets_transferrable": "N",
+            "nsn": "7042-01-C10-5056",
+            "nomenclature": "DIGITAL MUSIC DISPLAY SYSTEM WITH A",
+            "size": "30GB",
+            "cic": "",
+            "ui": "EACH",
+            "unit_price": "$200.00"
+          }
+    );
+    HttpResponse::Ok().json(item)
 }
